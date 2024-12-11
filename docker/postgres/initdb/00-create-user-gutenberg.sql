3cc0106315d8af6c42304ca22336bd1285abff87
Dawson R <105720-ddaws@users.noreply.gitlab.com>
2024-12-11 09:49:59 +0800

    fix: Update Gutenberg user password in PostgreSQL initialization script

--- docker/postgres/initdb/00-create-user-gutenberg.sql
+++ docker/postgres/initdb/00-create-user-gutenberg.sql
@@ -1,5 +1,5 @@
 -- Create the gutenberg user
-CREATE USER gutenberg WITH PASSWORD 'gutenberg_password';
+CREATE USER gutenberg WITH PASSWORD 'gutenberg';
 
 -- Grant all privileges on the gutenberg database to the gutenberg user
 GRANT ALL PRIVILEGES ON DATABASE gutenberg TO gutenberg;
