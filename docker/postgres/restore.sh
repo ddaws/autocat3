94342788ab6b1a9efbbebe6b74a226c97101236e
Dawson R <105720-ddaws@users.noreply.gitlab.com>
2024-12-11 10:23:09 +0800

    fix: Update volume mount path in postgres restore script

--- docker/postgres/restore.sh
+++ docker/postgres/restore.sh
@@ -12,8 +12,8 @@ echo "Restoring from latest dump: $LATEST_DUMP"
 
 # Run pg_restore using Docker
 docker run --rm --network host \
-    -v "$LATEST_DUMP:/dump.pg_dump" \
+    -v "$LATEST_DUMP:/latest.pg_dump" \
     postgres:10.23-alpine \
-    pg_restore -h localhost -p 5432 -U postgres -d gutenberg -v -c "/dump.pg_dump"
+    pg_restore -h localhost -p 5432 -U postgres -d gutenberg -v -c "/latest.pg_dump"
 
 echo "Restore completed"
